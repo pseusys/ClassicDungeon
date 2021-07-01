7dbbdc53eff13cd549d98aa8f6b9fac7e774eec9
milesartemius <shveitsar215@gmail.com>
2021-07-01 06:28:05 +0300

    float downshifting

--- shared/src/androidMain/kotlin/com/ekdorn/classicdungeon/shared/dependant/gl/GLFunctions.kt
+++ shared/src/androidMain/kotlin/com/ekdorn/classicdungeon/shared/dependant/gl/GLFunctions.kt
@@ -1,42 +1,9 @@
 package com.ekdorn.classicdungeon.shared.dependant.gl
 
 actual object GLFunctions {
-    actual enum class FILTERING_MODE {
-        NEAREST, LINEAR
+    actual fun setup() {
     }
 
-    actual enum class WRAPPING_MODE {
-        NEAREST, LINEAR
-    }
-
-    actual fun generate(): Int {
-        TODO("Not yet implemented")
-    }
-
-    actual fun activate(texture: Int) {
-    }
-
-    actual fun bind(texture: Int) {
-    }
-
-    actual fun release(texture: Int) {
-    }
-
-    actual fun delete(texture: Int) {
-    }
-
-    actual fun filter(
-        minification: FILTERING_MODE,
-        magnification: FILTERING_MODE
-    ) {
-    }
-
-    actual fun wrap(
-        s: WRAPPING_MODE,
-        t: WRAPPING_MODE
-    ) {
-    }
-
-    actual fun image(w: Int, h: Int, data: ByteArray) {
+    actual fun drawElements(count: Int, indices: ByteArray) {
     }
 }
